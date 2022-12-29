631f1af0a64be49ed32909eb8a9b4fc5fc0e3436
Emrah Nazif <emrah@ayofi.com>
2022-12-29 10:13:57 +0100

    remove functions from init

--- datamarkin/__init__.py
+++ datamarkin/__init__.py
@@ -1,20 +1,5 @@
-from datamarkin import messenger, config
+from .messenger import get_item
+from .messenger import api_key
 
-__version__ = "1.0.0.dev1"
+__version__ = "1.0.1.dev1"
 __author__ = 'Datamarkin'
-
-api_url = config.api_url
-api_key = config.api_key
-
-def status():
-    api_route = '/users/me'
-    url = api_url + api_route + '?access_token=' + api_key
-    print(url)
-    data = messenger.get_item(url)
-    return data
-
-
-class Status:
-    def __init__(self, user_id, plan_id):
-        self.id = user_id
-        self.plan_id = plan_id
