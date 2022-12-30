b4f04c8611d71c92eac2fb1480e6704ad848cefc
Emrah Nazif <emrah@ayofi.com>
2022-12-30 12:47:03 +0100

    add basic messenger functions

--- datamarkin/messenger.py
+++ datamarkin/messenger.py
@@ -2,21 +2,95 @@ import requests
 import os
 import magic
 import json
+import time
 from .config import api_url, api_key
+from tabulate import tabulate
+import datamarkin.core
+
+
+def check_resp(response):
+    if response.status_code == 200:
+        return response
+    else:
+        print("[INFO] API is not working. Status code :" + str(response.status_code))
+        return None
+
+
+def get_resp(url, headers=None, data=None, mode=1):
+    if mode == 1:
+        response = requests.get(url)
+
+    elif mode == 2:
+        response = requests.post(url, headers=headers, data=data)
+
+    elif mode == 3:
+        response = requests.patch(url, headers=headers, data=data)
+
+    elif mode == 4:
+        response = requests.delete(url, headers=headers, data=data)
+
+    else:
+        response = None
+
+    return response
+
+
+def waiting_response(url, headers=None, data=None, standby_time=1.0, mode=1):
+    """ This function to delay requests """
+    waiting_status_code_list = [404, 503]
+    time.sleep(standby_time)
+    response = get_resp(url, headers=headers, data=data, mode=mode)
+
+    while response.status_code in waiting_status_code_list:
+        if response.status_code == 404:
+            standby_time += 5
+            print("[INFO] API waiting. Status code : 404")
+            time.sleep(standby_time)
+            response = get_resp(url, headers=headers, data=data, mode=mode)
+            if standby_time > 30:
+                print("[INFO] Time out. Status code : 404")
+                response = check_resp(response)
+                break
+
+        elif response.status_code == 503:
+            standby_time = standby_time * (standby_time + 1)
+            print("[INFO] API waiting. Status code : 503")
+            time.sleep(standby_time)
+            response = get_resp(url, headers=headers, data=data, mode=mode)
+
+        else:
+            response = check_resp(response)
+
+    else:
+        response = check_resp(response)
+
+    return response
 
 
 def get_item(url):
     response = requests.get(url)
-    return response
+
+    if response.status_code != 200:
+        response = waiting_response(url, standby_time=0.1, mode=1)
+        return response
+
+    else:
+        return response
 
 
 def create_item(url, data):
     headers = requests.structures.CaseInsensitiveDict()
     headers["Accept"] = "application/json"
     headers["Content-Type"] = "application/json"
+
     response = requests.post(url, headers=headers, data=data)
 
-    return response
+    if response.status_code != 200:
+        response = waiting_response(url, headers=headers, data=data, standby_time=0.1, mode=2)
+        return response
+
+    else:
+        return response
 
 
 def update_item(url, data):
@@ -26,7 +100,12 @@ def update_item(url, data):
 
     response = requests.patch(url, headers=headers, data=data)
 
-    return response
+    if response.status_code != 200:
+        response = waiting_response(url, headers=headers, data=data, standby_time=0.1, mode=3)
+        return response
+
+    else:
+        return response
 
 
 def delete_item(url, data):
@@ -36,7 +115,12 @@ def delete_item(url, data):
 
     response = requests.delete(url, headers=headers, data=data)
 
-    return response
+    if response.status_code != 200:
+        response = waiting_response(url, headers=headers, data=data, standby_time=1, mode=4)
+        return response
+
+    else:
+        return response
 
 
 def upload_files(path_list):
@@ -65,7 +149,51 @@ def upload_file(path):
     return info_file
 
 
-def get_projects(items_per_request=-1, sort="", search_filter=""):
+def get_projects(items_per_request=-1, display=True):
+    url = f"{api_url}/items/projects?sort=sort,-date_created&limit={items_per_request}&page=1&access_token={api_key}"
+    response = get_item(url)
+    info_projects = json.loads(response.content)
+    num_projects = len(info_projects['data'])
+    data = []
+    col_names = ["Project Name", "Project ID", "Number of Class", "Project Type"]
+    for i in range(num_projects):
+        project_name = info_projects['data'][i]['name']
+        project_id = info_projects['data'][i]['id']
+        classes_info = info_projects['data'][i]['datasets']
+        project_type = info_projects['data'][i]['type']
+        data.append([project_name, project_id, len(classes_info), project_type])
+    if display:
+        print(tabulate(data, headers=col_names, tablefmt="pretty"))
+    else:
+        print('Number of Projects Found:::', num_projects)
+        return info_projects
+
+
+def get_project_by_id(project_id, deep={"datasets": {"_limit": 750}}):
+    deep = json.dumps(deep)
+    url = f"{api_url}/items/projects/{project_id}?access_token={api_key}&deep={deep}"
+    response = get_item(url)
+    info_project = json.loads(response.content)
+    project = datamarkin.core.Project(info_project['data']['id'],
+                                      info_project['data']['status'],
+                                      info_project['data']['user_created'],
+                                      info_project['data']['date_created'],
+                                      info_project['data']['user_updated'],
+                                      info_project['data']['date_updated'],
+                                      info_project['data']['name'],
+                                      info_project['data']['type'],
+                                      info_project['data']['train'],
+                                      info_project['data']['model_architecture'],
+                                      info_project['data']['description'],
+                                      info_project['data']['datasets'],
+                                      info_project['data']['configuration'],
+                                      info_project['data']['trainings'],
+                                      info_project['data']['augmentation'])
+    project.pre_inspection()
+    return project
+
+
+def new_get_projects(items_per_request=-1, sort="", search_filter=""):
     url = f"{api_url}/items/projects?sort={sort}&limit={items_per_request}&page=1&filter{search_filter}&access_token={api_key}"
     response = get_item(url)
 
@@ -95,30 +223,6 @@ def get_projects(items_per_request=-1, sort="", search_filter=""):
     return project_list
 
 
-def get_project_by_id(project_id, deep={"datasets": {"_limit": 750}}):
-    deep = json.dumps(deep)
-    url = f"{api_url}/items/projects/{project_id}?access_token={api_key}&deep={deep}"
-    response = get_item(url)
-    info_project = json.loads(response.content)
-    project = datamarkin.core.Project(info_project['data']['id'],
-                                      info_project['data']['status'],
-                                      info_project['data']['user_created'],
-                                      info_project['data']['date_created'],
-                                      info_project['data']['user_updated'],
-                                      info_project['data']['date_updated'],
-                                      info_project['data']['name'],
-                                      info_project['data']['type'],
-                                      info_project['data']['train'],
-                                      info_project['data']['model_architecture'],
-                                      info_project['data']['description'],
-                                      info_project['data']['datasets'],
-                                      info_project['data']['configuration'],
-                                      info_project['data']['trainings'],
-                                      info_project['data']['augmentation'])
-    project.pre_inspection()
-    return project
-
-
 def get_dataset_by_id(dataset_id):
     url = f"{api_url}/items/datasets/{dataset_id}?access_token={api_key}"
     response = get_item(url)
@@ -136,12 +240,28 @@ def get_dataset_by_id(dataset_id):
     return dataset
 
 
+def update_dataset():
+    pass
+
+
 def get_file_by_id(dataset_id, page=1):
-    limit = -1
     url = f"{api_url}/items/datasets_files?search={dataset_id}&limit={limit}&page={page}&access_token={api_key}"
     response = get_item(url)
     info_file = json.loads(response.content)
     return info_file
+
+
+def get_project_datasets():
+    print('datasets:')
+
+
+def get_dataset_files():
+    print('datasets:')
+
+
+def download_file(file_id, file_dir):
+    resp = get_file_request(file_id)
+    get_write_file_with_requests(resp, file_dir, file_id)
 
 
 def create_project(data):
@@ -266,3 +386,49 @@ def update_training(training_id, data):
     response = update_item(url, data)
     info_training = json.loads(response.content)
     return info_training
+
+
+def get_file_request(file_id, with_ext=False):
+    file_url = "https://api.datamarkin.com/assets/" + file_id + "?access_token=" + api_key
+
+    r = requests.get(file_url, stream=True)
+    if r.status_code == 200:
+        if with_ext:
+            ext = r.headers['content-type'].split('/')[-1]
+            return r, ext
+        else:
+            return r
+    else:
+        print("[INFO] API is not working. Status code :" + str(r.status_code))
+
+
+def get_write_file_with_requests(r, file_dir, file_id, ext=None):
+    if ext is not None:
+        with open(file_dir + file_id + '.' + ext, 'wb') as f:
+            f.write(r.content)
+    else:
+        with open(file_dir + file_id, 'wb') as f:
+            f.write(r.content)
+
+
+def get_list_of_items(collection, max_results=500, items_per_request=100, deep="", filter="filter", fields=""):
+    list_of_items = []
+    offset = 0
+    max_iterate = int(max_results / items_per_request)
+
+    for i in range(0, max_iterate):
+        if collection == "files":
+            url = f"{api_url}/{collection}?access_token={api_key}&limit={items_per_request}&offset={offset}&deep={deep}&{filter}&fields={fields}"
+        else:
+            url = f"{api_url}/items/{collection}?access_token={api_key}&limit={items_per_request}&offset={offset}&deep={deep}&{filter}&fields={fields}"
+
+        response = get_item(url)
+        response_content = json.loads(response.content)['data']
+        if len(response_content) > 0:
+            for item in response_content:
+                list_of_items.append(item)
+            offset = offset + items_per_request
+            print("[INFO]", len(list_of_items), "images found and counting...")
+        else:
+            break
+    return list_of_items
